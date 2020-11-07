335296d4ac12fc0ca27dfad6751a513bccc36662
realTobby <tkattanek00@gmail.com>
2020-11-07 20:53:06 +0100

    This is so comfy
    - created a menu for planting trees
    - created the first farm patch
    - can plant a tree on the farm patch
    - menu can open and close
    - added a cool screenshot to the progress folder

---
+++ Comfy-Farm-Sim/Assets/Scripts/Farmer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Farmer : MonoBehaviour
+{
+    MenuRendering menurenderer;
+
+    GameObject highlightedFarmpatch;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        menurenderer = GameObject.FindGameObjectWithTag("MENU_RENDERER").GetComponent<MenuRendering>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInputs();
+        
+
+
+    }
+
+    private void CheckInputs()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            highlightedFarmpatch = hit.transform.gameObject;
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            if(highlightedFarmpatch != null)
+            {
+                // open the farm menu
+                menurenderer.OpenFarmMenu(highlightedFarmpatch);
+            }
+        }
+
+        if (highlightedFarmpatch != null)
+        {
+            highlightedFarmpatch = null;
+        }
+
+    }
+}
---
+++ Comfy-Farm-Sim/Assets/Scripts/MenuRendering.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuRendering : MonoBehaviour
+{
+    private GameObject selectedFarmPatch;
+
+    public GameObject farmMenuPopup;
+
+    private PrefabCollection prefabList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        prefabList = GameObject.FindGameObjectWithTag("PREFAB_LIST").GetComponent<PrefabCollection>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenFarmMenu(GameObject clickedFarmPatch)
+    {
+        selectedFarmPatch = clickedFarmPatch;
+        farmMenuPopup.SetActive(true);
+    }
+
+    public void CloseFarmMenu()
+    {
+        farmMenuPopup.SetActive(false);
+    }
+
+    public void PlantTree()
+    {
+        GameObject newPlant = Instantiate(prefabList.treePrefab, selectedFarmPatch.transform.position, Quaternion.identity);
+        CloseFarmMenu();
+    }
+
+}
